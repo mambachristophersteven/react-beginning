5ea667b0cccd62edcf3bfd45009579bfec08e4cd
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-05 14:56:45 +0000

    folder structure

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    const title = "Hey, React. I will master to you, i swear."
+  return (
+    <div>
+        <h1>{title}</h1>
+    </div>
+  )
+}
+
+export default Header
