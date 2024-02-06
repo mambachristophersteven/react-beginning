1469d7b7b435b487152ab21dadef2ea23f04c401
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-06 13:34:58 +0000

    manipulating state of a component

--- src/Components/ShowWord/ShowWord.jsx
+++ src/Components/ShowWord/ShowWord.jsx
@@ -2,10 +2,17 @@ import React, { useState } from 'react'
 
 const ShowWord = () => {
     const [word,setWord] = useState('Grrrr');
-    console.log(useState('Grrrr'));
+
+    const handleClick = () =>{
+        setWord('king kong!!')
+    }
+    
 
   return (
-    <div>The current word state: {word}</div>
+    <div>
+        The current word state: {word}
+        <button onClick={handleClick}>Click me</button>
+    </div>
   )
 }
 
