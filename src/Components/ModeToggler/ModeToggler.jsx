cf33df47c8ee7e1ab846739a6f4bc4a342f4ed46
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-06 09:29:59 +0000

    using the not operator to get the handleClick function as well

--- src/Components/ModeToggler/ModeToggler.jsx
+++ src/Components/ModeToggler/ModeToggler.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 
 const ModeToggler = () => {
     let darkModeOn = true;
-    let darkMode = <h1>Dark mode is turned on</h1>
-    let lightMode = <h1>light mode is turned on</h1>
+    const darkMode = <h1>Dark mode is turned on</h1>
+    const lightMode = <h1>light mode is turned on</h1>
 
     const handleClick = () => {
-        if (darkModeOn === true){
-            darkModeOn = false;
-            console.log('light mode is on');
-        } else{
-            if(darkModeOn === false){
-                darkModeOn = true
-                console.log('dark mode is on');
-            }
+        darkModeOn =!darkModeOn;
+        if(darkModeOn === true){
+            console.log('dark mode is on');
+        }else{
+            console.log('light mode is on')
         }
     }
   return (
