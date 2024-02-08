aaccce7c8a8d77788de004476006e2359216d843
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-08 21:00:54 +0000

    making the functions to handle mathematical operations

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const Calculator = () => {
+    const inputRef = useRef(null);
+    const resultRef = useRef(null);
+    const [result, setResult] = useState(0);
+
+    const plus =(e) =>{
+        e.prevetDefault();
+        setResult((result) => result + Number(inputRef.current.value))
+    }
+    const minus =(e) =>{
+        e.prevetDefault();
+        setResult((result) => result - Number(inputRef.current.value))
+    }
+    const multiply =(e) =>{
+        e.prevetDefault();
+        setResult((result) => result * Number(inputRef.current.value))
+    }
+    const divide =(e) =>{
+        e.prevetDefault();
+        setResult((result) => result / Number(inputRef.current.value))
+    }
   return (
     <div className="calculator">
-        <p>calculator</p>
+        <h1>Simplest Working Calculator</h1>
+        <input type="number" name="number" id="" placeholder='Input a number'/>
+        <button>add</button>
     </div>
   )
 }
