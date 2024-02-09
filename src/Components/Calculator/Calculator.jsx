ab623e32455a20ffd6dabf4153f159b957fc68bf
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-09 10:44:17 +0000

    calculator content and logic done

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -6,46 +6,47 @@ const Calculator = () => {
     const [result, setResult] = useState(0);
 
     const plus =(e) =>{
-        e.prevetDefault();
+        e.preventDefault();
         setResult((result) => result + Number(inputRef.current.value));
     }
     const minus =(e) =>{
-        e.prevetDefault();
+        e.preventDefault();
         setResult((result) => result - Number(inputRef.current.value));
     }
     const multiply =(e) =>{
-        e.prevetDefault();
+        e.preventDefault();
         setResult((result) => result * Number(inputRef.current.value));
     }
     const divide =(e) =>{
-        e.prevetDefault();
+        e.preventDefault();
         setResult((result) => result / Number(inputRef.current.value));
     }
     const resetInput =(e) =>{
-        e.prevetDefault();
+        e.preventDefault();
         inputRef.current.value = 0;
     }
     const resetResult =(e) =>{
-        e.prevetDefault();
+        e.preventDefault();
         setResult((preVal) => preVal * 0);
     }
   return (
     <div className="calculator">
         <h1>Simplest Working Calculator</h1>
         <form>
-            <p ref={resultRef}>
-                {/* {add the value of the current total} */}
-            </p>
+            <p ref={resultRef}>{result}</p>
             <input 
             pattern='[0-9]' 
             type='number' 
             ref={inputRef} 
             placeholder='Type a number'
             />
+            <br />
             <button onClick={plus}>add</button>
             <button onClick={minus}>minus</button>
+            <br />
             <button onClick={multiply}>multiply</button>
             <button onClick={divide}>divide</button>
+            <br />
             <button onClick={resetInput}>clear</button>
             <button onClick={resetResult}>AC</button>
         </form>
