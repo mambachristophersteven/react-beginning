ae5f7dafd12eabcb1a7868ebf46c467722a96617
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-28 11:41:04 +0000

    initializing daja for accordion with the data file

---
+++ src/Pages/Accordion/data.js
@@ -0,0 +1,7 @@
+const data =[
+    {
+        id: 1,
+        question: "What is an accordion?",
+        answer: "An accordion is a feature used in organising and displaying content in a clean way on a webpage"
+    }
+]
