140d2275330ba1902623984364a695a53fef2c41
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-28 11:46:32 +0000

    finished data for accordion

--- src/Pages/Accordion/data.js
+++ src/Pages/Accordion/data.js
@@ -3,5 +3,22 @@ const data =[
         id: 1,
         question: "What is an accordion?",
         answer: "An accordion is a feature used in organising and displaying content in a clean way on a webpage"
-    }
-]
+    },
+    {
+        id: 2,
+        question: "The nurse understands that they need to acknowledge their pt's pain.  Which statement best shows the nurse's understanding of this principle?",
+        answer: "The statement `The pain in your arm is real and not in your imagination` best shows the nurse's understanding of acknowledging the patient's pain. This statement validates the patient's experience and reassures them that their pain is not dismissed or minimized. It demonstrates empathy and understanding towards the patient's subjective experience of pain"
+    },
+    {
+        id: 3,
+        question: "Checking our own prejudices and values at the door is vital to being a good nurse.",
+        answer: "It is important for nurses to check their own prejudices and values at the door in order to provide unbiased and patient-centered care. By setting aside personal beliefs and judgments, nurses can create a safe and inclusive environment for patients, respecting their autonomy and individuality. This allows for better communication, trust, and understanding between nurses and patients, ultimately leading to improved healthcare outcomes.."
+    },
+    {
+        id: 4,
+        question: "Looking annoyed and not making eye contact are nonverbal blockers.",
+        answer: "Yes, looking annoyed and avoiding eye contact can indeed be considered nonverbal blockers. Nonverbal communication involves body language, gestures, facial expressions, and other cues that are not expressed through words. These `blockers` can hinder effective communication, as they might signal disinterest, discomfort, or a lack of openness to the conversation or interaction."
+    },
+];
+
+export default data;
